174573c7fddfaf4bd867cbada4ede01e3b90f55d
alissoncavalcanti <alissoncavalcanti1@yahoo.com.br>
2025-08-09 12:32:50 -0300

    Complexificação do fetch e arrow functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,18 @@ const limit = 10;
 
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
-fetch(url).then(function(response){
-    console.log(response);
-})
-
-console.log("20")
+fetch(url)
+    .then ((response) => response.json())
+    // SAME AS:
+    // .then(function(response){
+    //     return response.json()
+    // })
+    .then(function(response) {
+        console.log(response)
+    })
+    .catch(function(error) {
+        console.error(error)
+    })
+    .finally(function() {
+        console.log('Requisição concluída')
+    })
