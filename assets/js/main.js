5271ef268db7ed75e19ef3ffc69a56764efc595c
alissoncavalcanti <alissoncavalcanti1@yahoo.com.br>
2025-08-09 14:52:41 -0300

    Estrutura de laço para trazer nomes dos pokémon

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,34 @@ const limit = 10;
 
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
+function convertPokemonToLi (pokemon) {
+    return `
+        <li class="species">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList');
+
 fetch(url)
     .then ((response) => response.json())
-    // SAME AS:
-    // .then(function(response){
-    //     return response.json()
-    // })
+    .then (function(response) {
+        return response.results
+    })
     .then(function(response) {
-        console.log(response)
+        for (let index = 0; index < response.length; index++) {
+            const pokemon = response[index];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+        }
     })
     .catch(function(error) {
         console.error(error)
@@ -18,3 +38,7 @@ fetch(url)
     .finally(function() {
         console.log('Requisição concluída')
     })
+
+
+    
+
